b5e8fe7f8a5962504c45d8fb2447b7427b054be8
Danh Nguyen <danhnguyen267@gmail.com>
2021-01-09 19:11:44 -0800

    Wrapped NotFound emoji

--- src/modules/views/NotFound.jsx
+++ src/modules/views/NotFound.jsx
@@ -5,7 +5,7 @@ import Typography from '@material-ui/core/Typography'
 const NotFound = () => (
     <Container maxWidth="md">
         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-            ❌ Page Not Found
+        <span role="img" aria-label="notfound">❌</span> Page Not Found
         </Typography>
     </Container>
 )
