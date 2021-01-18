c04b9fe921492bc38d6cbd5b0e0d6084249b015f
Danh Nguyen <danhnguyen267@gmail.com>
2021-01-18 14:58:55 -0800

    Edited React Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,78 @@
-import React, { Suspense, lazy } from 'react'
-import { toast } from 'react-toastify'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import { createBrowserHistory } from 'history'
-import Footer from './modules/components/Footer'
+import React, { Suspense, lazy } from "react";
+import ReactGA from "react-ga";
+import { toast } from "react-toastify";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { createBrowserHistory } from "history";
+import Footer from "./modules/components/Footer";
 
-import AppNavbar from './modules/components/AppNavbar'
-import HeroImage from './modules/components/HeroImage'
+import AppNavbar from "./modules/components/AppNavbar";
+import HeroImage from "./modules/components/HeroImage";
 
 // components
-const About = lazy(() => import('./modules/views/About'))
-const Contact = lazy(() => import('./modules/views/Contact'))
-const PhotoGallery = lazy(() => import('./modules/views/PhotoGallery'))
-const VegGallery = lazy(() => import('./modules/views/VegGallery'))
-const FridgeProductsGallery = lazy(() => import('./modules/views/FridgeProductsGallery'))
-const Testimonials = lazy(() => import('./modules/views/Testimonials'))
-const NotFound = lazy(() => import('./modules/views/NotFound'))
+const About = lazy(() => import("./modules/views/About"));
+const Contact = lazy(() => import("./modules/views/Contact"));
+const PhotoGallery = lazy(() => import("./modules/views/PhotoGallery"));
+const VegGallery = lazy(() => import("./modules/views/VegGallery"));
+const FridgeProductsGallery = lazy(() =>
+  import("./modules/views/FridgeProductsGallery")
+);
+const Testimonials = lazy(() => import("./modules/views/Testimonials"));
+const NotFound = lazy(() => import("./modules/views/NotFound"));
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
+
+const trackingID = "UA-158716028-2";
+history.listen((location) => {
+  ReactGA.initialize(trackingID, {
+    titleCase: false,
+    gaOptions: {
+      siteSpeedSampleRate: 100,
+    },
+  });
+  ReactGA.set({ page: location.pathname })
+  ReactGA.pageview(location.pathname)
+});
 
 toast.configure({
-  position: 'top-right',
+  position: "top-right",
   autoClose: 5000,
   hideProgressBar: false,
   closeOnClick: true,
   rtl: false,
   pauseOnFocusLoss: true,
   draggable: true,
-  pauseOnHover: true
-})
+  pauseOnHover: true,
+});
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
       <Router history={history}>
         <AppNavbar />
         <HeroImage />
-        <Suspense fallback={<LinearProgress style={{ padding: '10px' }} color="secondary" />}>
+        <Suspense
+          fallback={
+            <LinearProgress style={{ padding: "10px" }} color="secondary" />
+          }
+        >
           <Switch>
-            <Route exact path='/' component={About} />
-            <Route exact path='/contact' component={Contact} />
-            <Route exact path='/testimonials' component={Testimonials} />
-            <Route exact path='/grocery' component={PhotoGallery} />
-            <Route exact path='/vegetablesgallery' component={VegGallery} />
-            <Route exact path='/fridgegallery' component={FridgeProductsGallery} />
-            <Route component={NotFound}/>
+            <Route exact path="/" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/testimonials" component={Testimonials} />
+            <Route exact path="/grocery" component={PhotoGallery} />
+            <Route exact path="/vegetablesgallery" component={VegGallery} />
+            <Route
+              exact
+              path="/fridgegallery"
+              component={FridgeProductsGallery}
+            />
+            <Route component={NotFound} />
           </Switch>
         </Suspense>
         <Footer />
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
